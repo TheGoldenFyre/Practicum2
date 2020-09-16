5dfc6b122371dc903b1f85efad7ec7b4ed6aeaf7
Thomas van der Plas <t.d.vanderplas@students.uu.nl>
2020-09-16 18:38:11 +0200

    pow!

---
+++ Pow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+            Console.WriteLine(pow(x, e));
+        }
+
+        static double pow(int x, int e)
+        {
+            //TODO add 0^0 and x^negative support
+            
+            double tmp = 1;
+            if (x == 0)
+            {
+                tmp = 0;
+            }
+            for (int i = 0; i < e; i++)
+            {
+                tmp *= x;
+            }
+            return tmp;
+        }
+    }
+}
